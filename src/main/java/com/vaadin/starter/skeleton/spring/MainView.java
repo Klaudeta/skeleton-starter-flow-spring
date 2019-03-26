76a67c4e337815e86eb196c3c59a5f3613dcdb95
Klaudeta <klaudeta@vaadin.com>
2019-03-26 15:58:14 +0200

    reproduce crud editor issue with custom fields

--- src/main/java/com/vaadin/starter/skeleton/spring/MainView.java
+++ src/main/java/com/vaadin/starter/skeleton/spring/MainView.java
@@ -1,10 +1,12 @@
 package com.vaadin.starter.skeleton.spring;
 
 import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.ComponentUtil;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.crud.BinderCrudEditor;
 import com.vaadin.flow.component.crud.Crud;
 import com.vaadin.flow.component.crud.CrudEditor;
+import com.vaadin.flow.component.crud.CrudFilter;
 import com.vaadin.flow.component.crud.CrudVariant;
 import com.vaadin.flow.component.dependency.HtmlImport;
 import com.vaadin.flow.component.html.Div;
@@ -13,12 +15,17 @@ import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.component.upload.Upload;
 import com.vaadin.flow.data.binder.BeanValidationBinder;
 import com.vaadin.flow.data.binder.Binder;
+import com.vaadin.flow.data.provider.AbstractDataProvider;
 import com.vaadin.flow.data.provider.DataProvider;
 import com.vaadin.flow.data.provider.ListDataProvider;
+import com.vaadin.flow.data.provider.Query;
 import com.vaadin.flow.data.renderer.TemplateRenderer;
 import com.vaadin.flow.dom.Element;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Stream;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
@@ -29,72 +36,37 @@ import com.vaadin.flow.server.PWA;
 @PWA(name = "Project Base for Vaadin Flow with Spring", shortName = "Project Base")
 public class MainView extends VerticalLayout {
 
-    private static final String EDIT_COLUMN_KEY = "vaadin-crud-edit-column";
-    private final Crud<Person> crud;
+    public MainView() {
+        List<Person> personList = new ArrayList<>(new ArrayList<>(Arrays.asList(new Person(1, "MyName", "MySurname"))));
+        CrudDataProvider dataProvider = new CrudDataProvider(personList);
 
-    public MainView(@Autowired MessageBean bean) {
-
-        crud = new Crud<>(Person.class, createPersonEditor());
-
-        PersonDataProvider dataProvider = new PersonDataProvider();
+        Crud<Person> crud = new Crud<>(Person.class, createPersonEditor());
 
         crud.getGrid().removeColumnByKey("id");
-        crud.setDataProvider(dataProvider);
-        crud.addSaveListener(e -> dataProvider.persist(e.getItem()));
-        crud.addDeleteListener(e -> dataProvider.delete(e.getItem()));
-
-        crud.addThemeVariants(CrudVariant.NO_BORDER);
-
-
-
-//        crud.getGrid().addComponentColumn(person -> new CustomEditColumn<>(person)
-//                .visibleWhen(val -> val.getFirstName().startsWith("A"))
-//                .executeWhenClicked(() -> Notification.show(person.getFirstName())));
-
-        Crud.removeEditColumn(crud.getGrid());
-        crud.getGrid().addComponentColumn(person -> {
-            CustomEditColumn<Person> button = new CustomEditColumn<>(person);
-            button.visibleWhen(val -> !val.getFirstName().startsWith("A"));
-            button.executeWhenClicked(() -> {
-                crud.getEditor().setItem(person);
-                crud.setOpened(true);
-
-                // The two above lines with be replaced with this commented line below
-                // in the new release of Crud Flow component
-                // More details in https://github.com/vaadin/vaadin-crud-flow/pull/123
-//                crud.edit(person, Crud.EditMode.EXISTING_ITEM);
-            });
-
-            return button;
-        })
-        .setKey(EDIT_COLUMN_KEY)
-        .setWidth("4em")
-        .setFlexGrow(0);
+        crud.setDataProvider(dataProvider );
+        crud.addSaveListener(e -> dataProvider.saveItem(e.getItem()));
+        crud.addDeleteListener(e -> dataProvider.removeItem(e.getItem()));
 
         this.add(crud);
-
     }
+
 
     private BinderCrudEditor<Person> createPersonEditor() {
         BeanValidationBinder<Person> binder = new BeanValidationBinder<>(Person.class);
 
         Div form = new Div();
 
-
         TextField name = new TextField("Name");
         CustomField surname = new CustomField("Surname");
 
-        CrudField<Phone> phoneCrudField = new CrudField<>(Phone.class, createPhoneEditor());
-
-        form.add(name, surname, phoneCrudField);
+        form.add(name, surname);
 
         binder.bind(name, "firstName");
         binder.bind(surname, "lastName");
-        binder.bind(phoneCrudField, "phones");
 
         binder.addValueChangeListener(e -> {
-           Component component =  (Component)e.getHasValue();
-           // This JS execution trigger the change event on client side resulting
+            Component component =  (Component)e.getHasValue();
+            // This JS execution trigger the change event on client side resulting
             // in the Save button becoming dirty and so enabled.
             // This will allow all all custom fields used in the Crud Editor
             // to enable the Save button once their value is changed.
@@ -110,26 +82,90 @@ public class MainView extends VerticalLayout {
         };
     }
 
+    private class CrudDataProvider extends AbstractDataProvider<Person, CrudFilter> {
+        final List<Person> DATABASE;
 
-    private BinderCrudEditor<Phone> createPhoneEditor() {
-        BeanValidationBinder<Phone> binder = new BeanValidationBinder<>(Phone.class);
+        public CrudDataProvider(List<Person> items) {
+            DATABASE = items;
+        }
 
-        Div form = new Div();
+        @Override
+        public boolean isInMemory() {
+            return true;
+        }
 
-        TextField number = new TextField("Number");
+        @Override
+        public int size(Query<Person, CrudFilter> query) {
+            return DATABASE.size();
+        }
 
-        form.add(number);
+        @Override
+        public Stream<Person> fetch(Query<Person, CrudFilter> query) {
+            return DATABASE.stream();
+        }
 
-        binder.bind(number, "number");
-
-
-        return new BinderCrudEditor<Phone>(binder, form) {
-            @Override
-            public boolean isValid() {
-                return binder.validate().isOk();
+        public void saveItem(Person item){
+            if(!DATABASE.contains(item)){
+                DATABASE.add(item);
             }
-        };
+        }
+
+        public void removeItem(Person item){
+            DATABASE.remove(item);
+        }
+    }
+
+    public static class Person {
+        private Integer id;
+        private String firstName;
+        private String lastName;
+
+        public Person() {
+        }
+
+        public Person(Integer id, String firstName, String lastName) {
+            this.id = id;
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+        public String getFirstName() {
+            return firstName;
+        }
+
+        public void setFirstName(String firstName) {
+            this.firstName = firstName;
+        }
+
+        public String getLastName() {
+            return lastName;
+        }
+
+        public void setLastName(String lastName) {
+            this.lastName = lastName;
+        }
+
+        public Integer getId() {
+            return id;
+        }
+
+        public void setId(Integer id) {
+            this.id = id;
+        }
+
     }
 
 
 }
+
+
+
+//          binder.addValueChangeListener(e -> {
+//             Component component =  (Component)e.getHasValue();
+//             // This JS execution trigger the change event on client side resulting
+//              // in the Save button becoming dirty and so enabled.
+//              // This will allow all all custom fields used in the Crud Editor
+//              // to enable the Save button once their value is changed.
+//
+//              component.getElement().executeJavaScript("setTimeout(function(){$0.dispatchEvent(new CustomEvent('change', {bubbles: true, composed: true}));},0)", component.getElement());
+//          });
