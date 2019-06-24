990eb0afd8222fea37f28deb9bd4367fb75f9c2e
Klaudeta <klaudeta@vaadin.com>
2019-06-24 11:48:22 +0300

    simplify the testcase

--- src/main/java/com/vaadin/starter/skeleton/spring/TestDialog.java
+++ src/main/java/com/vaadin/starter/skeleton/spring/TestDialog.java
@@ -1,32 +1,35 @@
 package com.vaadin.starter.skeleton.spring;
 
+import com.vaadin.flow.component.AttachEvent;
 import com.vaadin.flow.component.dialog.Dialog;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.component.treegrid.TreeGrid;
 import java.util.concurrent.Executors;
 
-public class TestDialog extends Dialog {
+public class TestDialog extends Notification {
 
     public TestDialog() {
-        loadGrid();
+
     }
 
     private void loadGrid() {
-        getElement().getNode().runWhenAttached(ui -> {
 
-            Executors.newSingleThreadExecutor().execute(() -> {
-
-                TreeGrid<MyUser> grid = new TreeGrid<>();
-                grid.setWidth("400px");
-                grid.setHeight("300px");
-                grid.addColumn(MyUser::getEmail).setHeader("Email");
-
-//                grid.setItems(users);
-
-                ui.access(() -> {
+                getUI().get().access(() -> {
+                    Grid<MyUser> grid = new Grid<>();
+                    grid.setWidth("400px");
+                    grid.setHeight("300px");
+                    grid.addColumn(MyUser::getEmail).setHeader("Email");
                     add(grid);
                 });
-            });
-        });
+    }
+
+    @Override
+    protected void onAttach(AttachEvent attachEvent) {
+        super.onAttach(attachEvent);
+        loadGrid();
     }
 
     public static class MyUser{
